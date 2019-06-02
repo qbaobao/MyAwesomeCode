f6752f26a178ac402cc4a29c76a0ac685fe280fa
qbaobao <mxq123345@163.com>
2019-06-02 11:15:19 +0800

    add BaseCode

---
+++ base_code/baseCode.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "baseCode.h"
+
+using namespace std;
+int main()
+{
+    return;
+}
---
+++ base_code/baseCode.h
@@ -0,0 +1,4 @@
+#pragma once
+#ifndef RED_BACK_TREE_H
+#define RED_BACK_TREE_H
+#endif // !RED_BACK_TREE_H
