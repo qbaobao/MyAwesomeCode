44ebecd156ce7110c2b991067148aa443e70cd70
qbaobao <mxq123345@163.com>
2019-10-07 23:01:03 +0800

    update

--- leetcode/31. Next Permutation.cpp
+++ leetcode/31. Next Permutation.cpp
@@ -8,10 +8,11 @@ public:
         for (int i = nums.size() - 1; i > 0; i--) {
             if (nums[i] > nums[i - 1]) {
                 flag = true;
-                for (int j = nums.size() - 1; j >= i && nums[j] <= nums[i - 1]; j--) ;
-                nums[i] ^= nums[j];
-                nums[j] ^= nums[i];
-                nums[i] ^= nums[j];
+                int j = nums.size() - 1;
+                for (; j >= i && nums[j] <= nums[i - 1]; j--) ;
+                nums[i - 1] ^= nums[j];
+                nums[j] ^= nums[i - 1];
+                nums[i - 1] ^= nums[j];
                 sort(nums.begin() + i, nums.end());
                 break;
             }
